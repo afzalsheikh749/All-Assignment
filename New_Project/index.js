8a854a51fb2d6829b573a433c0c54e1ad5a9bda4
Afzal Hussain <mdafzal297922@gmail.com>
2025-10-28 17:19:04 +0500

    create new function

--- New_Project/index.js
+++ New_Project/index.js
@@ -1,5 +1,11 @@
 function studentName(name) {
     console.log(`My name is ${name}`)
 }
-
 studentName("Afzal")
+
+
+function studentName(name) {
+   alert(`My name is ${name}`)
+}
+studentName("Afzal")
+
